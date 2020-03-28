1d2e1a599395736c19d2d061ae85db6ddcb325ba
Claudio Pereira <claudiofrancopereira@gmail.com>
2020-03-28 01:06:11 -0300

    mobile da aplicacao

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -20,6 +20,7 @@ export default function Profile()
     useEffect(() => {
         api.get('profile', 
         {
+            
             headers: 
             {
                 Authorization: ongID,
